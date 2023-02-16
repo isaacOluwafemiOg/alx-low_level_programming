a8ed3c276a58ea3a23b757aebe43673468194cb9
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-16 15:46:39 -0800

    script to print out size  of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	char myarr[5][] = ["char", "int", "long int", "long long int", "float"];
+	int i;
+	for (i=0; i<5; i++)
+	{
+		printf("Size of a %s: %d byte(s)\n", myarr[i], sizeof(myarr[i]));
+	}
+	return (0);
+}	
