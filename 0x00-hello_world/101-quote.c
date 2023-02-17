cc4b546c45df77b93aba26e8c1ce84f174660749
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-16 17:10:42 -0800

    print to stderr using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora, 2015-10-19";
+
+	write(2, str, strlen(str));
+
+	return (1);
+}
