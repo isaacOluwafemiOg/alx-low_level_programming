4bc4e7828660f263d9dd804dd2b284a7da7d86db
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-17 10:21:27 -0800

    script to print lowercase and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * Description: This program uses putchar to
+ * print all the english alphabets in lower
+ * and uppercase
+ *
+ * Return: Always returns 0 (success)
+ */
+
+int main(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+	int i;
+
+	for (i = 0; i < strlen(alphabets); i++)
+	{
+		putchar(alphabets[i]);
+	}
+
+	return (0);
+}
