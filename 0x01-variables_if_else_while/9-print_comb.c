e835b62a629b7be0c2dc7271b3938831545cac9c
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-17 12:55:23 -0800

    script to print combination of decimal digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * Description: This program uses putchar to
+ * print all the digits of the decimal
+ * system separated by a comma and whitespace
+ * without making use of a char type
+ * variable
+ *
+ * Return: Always returns 0 (success)
+ */
+
+int main(void)
+{
+
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('9');
+
+	return (0);
+}
