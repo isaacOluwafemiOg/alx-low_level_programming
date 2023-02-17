b61f316ab0b87cc1eb1f7ba8a2185682551ba278
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-17 10:59:53 -0800

    script to print decimal digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ * Description: This program uses putchar to
+ * print all the digits of the decimal
+ * system
+ *
+ * Return: Always returns 0 (success)
+ */
+
+int main(void)
+{
+	char digits[] = "0123456789\n";
+	size_t i;
+
+	for (i = 0; i < strlen(digits); i++)
+	{
+		putchar(digits[i]);
+	}
+
+	return (0);
+}
