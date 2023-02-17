495b23fc2c96cd40c74646e6953e68283f6ab98a
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-16 16:15:47 -0800

    debugging 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	char myarr[][5] = {"char", "int", "long int", "long long int", "float"};
-	int i;
-	for (i=0; i<5; i++)
-	{
-		printf("Size of a %s: %d byte(s)\n", myarr[i], sizeof(myarr[i]));
-	}
+	
+	
+	printf("Size of a %s: %d byte(s)\n", "char", sizeof(char);
+	printf("Size of a %s: %d byes(s)\n", "int", sizeof(int);	
+	printf("Size of a %s: %d byes(s)\n", "long int", sizeof(long int);
+	printf("Size of a %s: %d byes(s)\n", "long long int", sizeof(long long int);
+	printf("Size of a %s: %d byes(s)\n", "float", sizeof(float);
 	return (0);
+
+	
 }	
