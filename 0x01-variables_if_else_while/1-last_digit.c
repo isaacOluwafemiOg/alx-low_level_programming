bed93746db4e1062fef4f141af7df5203f1278e0
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-17 07:32:53 -0800

    debugging compile time error in 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,8 +19,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int v = n%10;
-	printf("Last digit of %d is ", v)
+	int v = n % 10;
+	printf("Last digit of %d is ", v);
 	if (v > 5)
 	{
 		printf("and is greater than 5\n");
