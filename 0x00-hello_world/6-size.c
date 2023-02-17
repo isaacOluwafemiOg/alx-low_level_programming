a803ce4644570a7a1e3ee66b94df6593e533aca4
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-16 16:26:57 -0800

    correcting typo and betty coding style compliance

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@ int main(void)
 {
 
 	printf("Size of a %s: %d byte(s)\n", "char", sizeof(char));
-	printf("Size of a %s: %d byte(s)\n", "int", sizeof(int));
+	printf("Size of an %s: %d byte(s)\n", "int", sizeof(int));
 	printf("Size of a %s: %d byte(s)\n", "long int", sizeof(long int));
 	printf("Size of a %s: %d byte(s)\n", "long long int", sizeof(long long int));
 	printf("Size of a %s: %d byte(s)\n", "float", sizeof(float));
 	return (0);
-}	
+}
