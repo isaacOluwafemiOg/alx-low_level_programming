4d94c16e506eb796f2ae13c2467cd0a4778c5ba2
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-21 12:36:29 -0800

    script to check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks whether an input value is lowercase or not
+ *
+ * Return: returns 1 when the input is lowercase and 0 if otherwise
+ *
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
