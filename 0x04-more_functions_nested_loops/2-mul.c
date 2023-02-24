167d10e632e34b1826258cf96f42323e060ccfec
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-24 03:55:33 -0800

    program to multiply two inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two input integers
+ *
+ * @a: first input to multipy
+ * @b: second input for multiplication
+ *
+ * Return: returns result of multiplication of two inputs
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,11 @@
 int _isupper(int);
 int _isdigit(int c);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagoanl(int);
+void print_square(int);
+void print_triangle(int);
+
