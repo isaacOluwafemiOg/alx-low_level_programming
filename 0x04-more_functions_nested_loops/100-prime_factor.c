e25a06f9a3347e97fad9502c169410afe8d06c22
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-24 14:58:47 -0800

    removed infinite loop in 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry to program
- *
  * prime_check - finds out if an input is prime or not
+ *
  * @v: the input to prime_check
- * Return: returns 0 always
+ *
+ * Return: returns 0 if number is not prime otherwise 1
  */
-
 
 
 int prime_check(int v)
@@ -23,25 +22,27 @@ int prime_check(int v)
 	return (1);
 }
 
+/**
+ * main - entry to program
+ *
+ * Description: makes use of prime_check find largest prime factor
+ * Return: returns 0 always
+ */
 int main(void)
 {
 	int i;
 	long int j;
 
 	j = 612852475143;
-	while (i >= 0)
+	for (i = 2; i < j; i++)
 	{
-		for (i = 2; i < j; i++)
+		if (j % i == 0 && prime_check(i) == 1)
 		{
-			if (j % i == 0 && prime_check(i) == 1)
-			{
-				j = j / i;
-				break;
-			}
+			j = j / i;
 		}
 	}
 	printf("%ld", j);
-
+	putchar('\n');
 	return (0);
 }
 
