1035659f50bc5f2ebd3e8fdbcf5bdeb523f072d0
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-24 06:26:47 -0800

    program to print 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - prints the numbers 0 to 14 ten times followed by a new line
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	int my[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '0', '1', '1', '1', '2', '1', '3', '1', '4'}
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 20; j++)
+		{
+			putchar(my[j]);
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+}
