f4cb15123981a628708d25294e64b6508ab11e28
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-24 05:06:26 -0800

    program to print decimal digits with exeption of 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints decimal digits followed by a new line
+ * with the execption of 2 and 4
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	int my[] = {'0', '1', '3', '5', '6', '7', '8', '9'};
+	for (i = 0; i < 9; i++)
+	{
+		putchar(my[i]);
+	}
+	putchar('\n');
+}
