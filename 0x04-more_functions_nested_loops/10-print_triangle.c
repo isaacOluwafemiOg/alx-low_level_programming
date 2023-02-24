7aad0ba802d0a57500cc4112d2354422036c1e3c
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-24 13:03:07 -0800

    print triangles using pounds

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - draws a triangle using a number of pounds
+ * @size: the input
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k, l;
+
+	i = size;
+	j = 1;
+
+	while (i > 0)
+	{
+		for (k = size - j; k > 0; k--)
+		{
+			putchar(' ');
+		}
+		for (l = 0; l < j; l++ )
+		{
+			putchar('#');
+		}
+		i--;
+		j++;
+		putchar('\n');
+	}
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+}
