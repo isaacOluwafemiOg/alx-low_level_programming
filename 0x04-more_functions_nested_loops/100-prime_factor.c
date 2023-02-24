7d381e91a47d6f05120e32bc11f96c65b4adb23e
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-24 14:28:26 -0800

    prime factor of a huge number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - entry to program
+ *
+ * Return: returns 0 always
+ */
+
+
+
+int prime_check(int v)
+{
+	int x;
+
+	for (x = 2; x < v; x++)
+	{
+		if (v % x == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int main(void)
+{
+	int i;
+	long int j;
+
+	j = 612852475143;
+	while (i >= 0)
+	{
+		for (i = 2; i < j; i++)
+		{
+			if (j % i == 0 && prime_check(i) == 1)
+			{
+				j = j / i;
+				break;
+			}
+		}
+	}
+	printf("%ld", j);
+
+	return (0);
+}
+
