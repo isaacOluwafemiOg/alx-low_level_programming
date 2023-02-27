aced5f75eaad6a5cebe218dfb8cd42fba81fef32
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-27 15:01:25 -0800

    function to calculate string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - calculates the lenght of a string.
+ *
+ * @s: the input string to the function
+ * Return: returns nothing
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	
+	i =0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
