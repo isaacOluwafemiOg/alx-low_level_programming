dd6171f3a7e4cc9a55514cd4a12850d4496d92b0
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-27 13:36:03 -0800

    function to carry out dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * to 98
+ *
+ * @n: the input to the function
+ *
+ * Return: returns nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
