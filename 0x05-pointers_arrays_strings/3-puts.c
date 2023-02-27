2f539a419d2f13648663203b8899a03ca5629e80
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-02-27 15:16:09 -0800

    puts function clone

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: the input string to the function
+ * Return: returns nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i];
+	}
+	putchar('\n');
+}
