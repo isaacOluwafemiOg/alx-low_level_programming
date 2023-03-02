bbcdaa4e3c10dae6bebc65e39e839c71fa4600b6
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-02 08:12:40 -0800

    function to imitate strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two input strings
+ * @s1: first string to compare
+ * @s2: second string to compare with
+ *
+ * Return: returns 15 if s1 >s2, -15 if s1 < s2, else 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	size_t count;
+
+	count = 0;
+
+	while (count < strlen(s1))
+	{
+		if (*s1++ < *s2++)
+		{
+			return (-15);
+		}
+		else if (*s1++ > *s2++)
+		{
+			return (15);
+		}
+		count++;
+	}
+	return (0);
+}
