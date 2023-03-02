e9888e721da96d54f400485e57d186226e248ab1
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-02 07:04:31 -0800

    added description for return value of _strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,8 @@
  * @dest: first string input
  * @src: string input to copy into the beginning of dest
  * @n: number of bytes of the src string to copy
+ *
+ * Return: returns the result of copying n characters from src to the beginning of dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
