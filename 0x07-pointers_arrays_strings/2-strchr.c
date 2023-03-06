68c8ecc11421c275f28fb0b7e83066ba48586989
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-06 10:56:43 -0800

    imitation of strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - lets you know the first occurance of c in the string s
+ * @s: the string in which to check for c
+ * @c: the character to check for in s
+ *
+ * Return: a pointer to the first occurrence of char c in s else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *v;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		v = 0;
+	}
+	else
+	{
+		v = &(s[i]);
+	}
+	return (v);
+}
