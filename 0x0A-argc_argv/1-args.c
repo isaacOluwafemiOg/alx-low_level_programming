cb6954c03929eaa5edf9774f8412c4a53367887a
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-10 11:02:25 -0800

    illustration of argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - entry to program
+ * @argc: number of arguments passed to main
+ * @argv: argument vector
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
