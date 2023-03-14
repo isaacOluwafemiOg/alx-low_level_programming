1ab03b3f9ffcd750553235cbfc88e5b347779ed8
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-14 14:13:08 -0700

    debugging 3-grid_alloc.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,7 @@ int **alloc_grid(int width, int height)
 	if (v == NULL)
 	{
 		free(v);
+		v = NULL;
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -32,6 +33,7 @@ int **alloc_grid(int width, int height)
 		if (v[i] == NULL)
 		{
 			free(v[i]);
+			v[i] = NULL;
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
