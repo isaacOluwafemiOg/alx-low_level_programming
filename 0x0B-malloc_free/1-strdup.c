b8a5b79ff2ae0bb6cb9a914e9d359419aac27728
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-14 09:33:10 -0700

    imitation of strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * strdup - creates a copy of string input
+ * @str: string input to copy
+ *
+ * Return: pointer to copied string
+ */
+
+char *_strdup(char *str)
+{
+	char *v;
+	unsigned int i, j;
+
+	j = strlen(str);
+
+	v = malloc(j + 1);
+	if (v == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	while (i < j)
+	{
+		v[i] = str[i];
+		i++;
+	}
+	v[j] = '\0';
+
+	return (v);
+}
