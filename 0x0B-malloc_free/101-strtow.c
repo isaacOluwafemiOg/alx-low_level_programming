7c520290b441bc23504763fc9091e2614b5de382
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-14 19:42:43 -0700

    debugging 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,6 +4,40 @@
 #include <ctype.h>
 #include <string.h>
 
+
+
+/**
+ * next_step - subfunction to put divided words into a single array
+ * @q: array showing size of each word in the full string
+ * @c: number of words in the string
+ * @r: position of the start of each word
+ * @str: string to split
+ * Return: pointer to the array of split words
+ */
+
+char **next_step(int *q, int c, int *r, char *str)
+{
+	char **v;
+	int j, i, s;
+	int *u;
+
+	v = malloc(sizeof(u) * c);
+	for (i = 0; i < c; i++)
+	{
+		v[i] = (char *)malloc(q[i]);
+		j = 0;
+		s = r[i];
+		while (str[s] != ' ')
+		{
+			v[i][j] = str[s];
+			s++;
+			j++;
+		}
+		v[i][j] = '\0';
+	}
+	return (v);
+}
+
 /**
  * strtow - splits a string into words
  * @str: string to be split
@@ -13,11 +47,9 @@
 
 char **strtow(char *str)
 {
-	char **v;
-	char *u;
 	int *q;
 	int *r;
-	unsigned int c, i, s, j;
+	unsigned int c, i;
 	char w;
 
 	if (str == NULL || strlen(str) == 0)
@@ -34,21 +66,12 @@ char **strtow(char *str)
 		{
 			r[c] = i;
 			c++;
+			while (str[i] != ' ')
+			{
+				i++;
+			}
+			q[c - 1] = i - r[c];
 		}
 	}
-	v = malloc(sizeof(u) * c);
-	for (i = 0; i < c; i++)
-	{
-		v[i] = (char *)malloc(q[i] + 1);
-		j = 0;
-		s = r[i];
-		while (str[s] != ' ')
-		{
-			v[i][j] = str[s];
-			s++;
-			j++;
-		}
-		v[j] = '\0';
-	}
-	return (v);
+	return (next_step(q, c, r, str));
 }
