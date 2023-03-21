77cd9e01eec32f550f3cf6d41520811abf5885f6
isaacOluwafemiOg <isaacogunniyi1@gmail.com>
2023-03-21 06:12:27 -0700

    first task on structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - just a structure
+ *
+ * @name - name of dog
+ * @age - dog's age
+ * @owner - name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
